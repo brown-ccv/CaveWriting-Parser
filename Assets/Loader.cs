74cffa612c906a8d777f44bcad403c704493153a
RobertGemmaJr <rgemma.jr@gmail.com>
2022-02-03 17:38:39 -0500

    misc changes

--- Assets/Loader.cs
+++ Assets/Loader.cs
@@ -1,10 +1,9 @@
 using UnityEngine;
-
 public class Loader : MonoBehaviour
 {
     private void Start()
     {
-        T scene = Xml.Deserialize<Hero>("./Visualizing_Rafson_Schwartz_Spr15/run.xml");
-        Debug.Log(scene);
+        Story story = XmlSerializer.Deserialize("./Visualizing_Rafson_Schwartz_Spr15/run.xml");
+        Debug.Log(story);
     }
 }
