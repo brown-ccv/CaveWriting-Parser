186f5631963ca51224567f31fd27b38df1c511c5
RobertGemmaJr <rgemma.jr@gmail.com>
2022-02-11 10:57:22 -0500

    chore: comment out script to not accidentally run it

--- Assets/add_namespaces.sh
+++ Assets/add_namespaces.sh
@@ -3,15 +3,17 @@
 cd "Classes/"
 
 # Append text to beginning of each cs file
-for f in *.cs; do 
-    echo "using UnityEngine;" > tmpfile
-    echo "using System.Xml.Serialization;" >> tmpfile
-    echo >> tmpfile
-    cat $f >> tmpfile
-    mv tmpfile $f
-done
+
+# for f in *.cs; do 
+#     echo "using UnityEngine;" > tmpfile
+#     echo "using System.Xml.Serialization;" >> tmpfile
+#     echo >> tmpfile
+#     cat $f >> tmpfile
+#     mv tmpfile $f
+# done
 
 # Remove first line from each cs file
+
 # for f in *.cs; do
 #     tail -n +2 "$f" > "$f.tmp" && mv "$f.tmp" "$f"
 # done
