fa53eba316c3c6d61431e969f3b1d63f8d5db6c7
RobertGemmaJr <rgemma.jr@gmail.com>
2022-02-15 15:27:14 -0500

    chore: clean up meta files

--- Assets/Loader.cs
+++ Assets/Loader.cs
@@ -7,7 +7,6 @@ using System.Collections.Generic; // TEMP
 
 public class Loader : MonoBehaviour
 {
-    // TODO: Delete run - copy.xml
     private void Start()
     {
 
@@ -17,38 +16,38 @@ public class Loader : MonoBehaviour
         );
 
         // TEMP - Test serialization with Color and Vector3
-        {
-            Story example = new Story();
-            example.ObjectRoot = new ObjectRoot();
-            example.ObjectRoot.Objects = new List<Object>(){ new Object()};
-            example.ObjectRoot.Objects[0].Name = "test object";
-            example.ObjectRoot.Objects[0].Visible = true;
-            example.ObjectRoot.Objects[0].Color = Color.white;
-            example.ObjectRoot.Objects[0].Lighting = false;
-            example.ObjectRoot.Objects[0].ClickThrough = false;
-            example.ObjectRoot.Objects[0].AroundSelfAxis = false;
-            example.ObjectRoot.Objects[0].Scale = 2.5;
-            example.ObjectRoot.Objects[0].Placement = new Placement();
-            example.ObjectRoot.Objects[0].Placement.RelativeTo = "Center";
-            example.ObjectRoot.Objects[0].Placement.Position = new Vector3(0.0f, 1.5f, -3.25f);
-            example.ObjectRoot.Objects[0].Content = new Content();
-            example.ObjectRoot.Objects[0].Content.Text = new Text();
-            example.ObjectRoot.Objects[0].Content.Text.Content = "IF I TOLD HIM";
-            example.ObjectRoot.Objects[0].Content.Text.HorizAlign = "center";
-            example.ObjectRoot.Objects[0].Content.Text.VertAlign = "center";
-            example.ObjectRoot.Objects[0].Content.Text.Font = "./fonts/Lucida.ttf";
-            example.ObjectRoot.Objects[0].Content.Text.Depth = 15;
-            PrintObject(example);
+        // {
+        //     Story example = new Story();
+        //     example.ObjectRoot = new ObjectRoot();
+        //     example.ObjectRoot.Objects = new List<Object>(){ new Object()};
+        //     example.ObjectRoot.Objects[0].Name = "test object";
+        //     example.ObjectRoot.Objects[0].Visible = true;
+        //     example.ObjectRoot.Objects[0].Color = Color.white;
+        //     example.ObjectRoot.Objects[0].Lighting = false;
+        //     example.ObjectRoot.Objects[0].ClickThrough = false;
+        //     example.ObjectRoot.Objects[0].AroundSelfAxis = false;
+        //     example.ObjectRoot.Objects[0].Scale = 2.5;
+        //     example.ObjectRoot.Objects[0].Placement = new Placement();
+        //     example.ObjectRoot.Objects[0].Placement.RelativeTo = "Center";
+        //     example.ObjectRoot.Objects[0].Placement.Position = new Vector3(0.0f, 1.5f, -3.25f);
+        //     example.ObjectRoot.Objects[0].Content = new Content();
+        //     example.ObjectRoot.Objects[0].Content.Text = new Text();
+        //     example.ObjectRoot.Objects[0].Content.Text.Content = "IF I TOLD HIM";
+        //     example.ObjectRoot.Objects[0].Content.Text.HorizAlign = "center";
+        //     example.ObjectRoot.Objects[0].Content.Text.VertAlign = "center";
+        //     example.ObjectRoot.Objects[0].Content.Text.Font = "./fonts/Lucida.ttf";
+        //     example.ObjectRoot.Objects[0].Content.Text.Depth = 15;
+        //     PrintObject(example);
 
-            Stream stream = File.Create(Application.dataPath + "/xml/test_write.xml");
-            serializer.Serialize(stream, example);
-            stream.Close();
+        //     Stream stream = File.Create(Application.dataPath + "/xml/test_write.xml");
+        //     serializer.Serialize(stream, example);
+        //     stream.Close();
 
-            using (StreamReader reader = new StreamReader(Application.dataPath + "/xml/run_test_write.xml"))
-            {
-                PrintObject((Story)serializer.Deserialize(reader));
-            }
-        }
+        //     using (StreamReader reader = new StreamReader(Application.dataPath + "/xml/run_test_write.xml"))
+        //     {
+        //         PrintObject((Story)serializer.Deserialize(reader));
+        //     }
+        // }
         
 
 
@@ -56,9 +55,9 @@ public class Loader : MonoBehaviour
         Story story;
         using (StreamReader reader = new StreamReader(Application.dataPath + "/xml/run_example.xml"))
         {
-            // story = (Story)serializer.Deserialize(reader);
+            story = (Story)serializer.Deserialize(reader);
         }
-        // PrintObject(story);
+        PrintObject(story);
 
         // Quit
         Application.Quit();
